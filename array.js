63cb2572331eb6235a9c9fc04bbf3ea1ab6f7634
Luís Nascimento de Sousa <luisrocksousa1111@gmail.com>
2024-03-18 23:11:46 -0300

    iniciando o estudo de estrutura de dados

---
+++ array.js
@@ -0,0 +1,17 @@
+//Essa é uma fora de fazer um array
+var mes = []
+
+mes[0] = 'janeiro'
+mes[1] = 'fevereiro'
+mes[2] = 'março'
+mes[3] = 'abril'
+mes[4] = 'maio'
+mes[5] = 'junho'
+mes[6] = 'julho'
+mes[7] = 'agosto'
+mes[8] = 'setembro'
+mes[9] = 'outubro'
+mes[10] = 'novembro'
+mes[11] = 'dezembro'
+
+console.log(mes[1])
