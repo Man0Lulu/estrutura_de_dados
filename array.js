19d6910da1f46da5896cf5c29cdd9c1ea162d57e
Luís Nascimento de Sousa <luisrocksousa1111@gmail.com>
2024-03-20 15:34:29 -0300

    acessando elemntos atraves da iteração

--- array.js
+++ array.js
@@ -14,10 +14,25 @@ mes[9] = 'outubro'
 mes[10] = 'novembro'
 mes[11] = 'dezembro'
 
-console.log(mes[1])
+//console.log(mes[1])
 
 //novo jeito de fazer array
 var diasdasemana = new Array('segunda-feira', 'teça-feira', 'quarta-feira',
 'quinta-feira', 'sexta-feira', 'sabado', 'domingo')
 
-console.log(diasdasemana[5])
+//acessando elementos de um array
+for(var i = 0; i < diasdasemana.length; i++){
+    console.log(diasdasemana[i])
+}
+
+
+var fibonnaci = []
+
+fibonnaci[0] = 0
+fibonnaci[1] = 1
+fibonnaci[2] = 2
+//acessando elementos atraves da iteração
+for(var f = 3; f < 20; f++ ){
+      fibonnaci[f] = fibonnaci[f - 1] + fibonnaci[f - 2] 
+}
+console.log(fibonnaci)
